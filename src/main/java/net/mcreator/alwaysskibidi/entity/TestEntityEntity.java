120346fa8e00a632b52468bec452b5d0b1039755
KK1212KK <Ke346@DESKTOP-6MQJD22.lan>
2024-09-06 23:39:49 -0400

    Idk what i added, just uploading before going to bed.

--- src/main/java/net/mcreator/alwaysskibidi/entity/TestEntityEntity.java
+++ src/main/java/net/mcreator/alwaysskibidi/entity/TestEntityEntity.java
@@ -1,37 +1,59 @@
 
 package net.mcreator.alwaysskibidi.entity;
 
-import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.neoforged.neoforge.event.entity.SpawnPlacementRegisterEvent;
+
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.ServerLevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.monster.Monster;
+import net.minecraft.world.entity.ai.goal.target.HurtByTargetGoal;
+import net.minecraft.world.entity.ai.goal.RandomStrollGoal;
+import net.minecraft.world.entity.ai.goal.RandomLookAroundGoal;
+import net.minecraft.world.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.world.entity.ai.goal.FloatGoal;
 import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.nbt.Tag;
-import net.minecraft.network.syncher.EntityDataAccessor;
+import net.minecraft.world.entity.ai.attributes.AttributeSupplier;
+import net.minecraft.world.entity.SpawnPlacementTypes;
+import net.minecraft.world.entity.SpawnGroupData;
+import net.minecraft.world.entity.MobSpawnType;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.DifficultyInstance;
+import net.minecraft.world.Difficulty;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.BuiltInRegistries;
+
+import net.mcreator.alwaysskibidi.procedures.TestEntityOnInitialEntitySpawnProcedure;
+import net.mcreator.alwaysskibidi.init.AlwaysSkibidiModEntities;
+
+import javax.annotation.Nullable;
 
 public class TestEntityEntity extends Monster {
-
 	public TestEntityEntity(EntityType<TestEntityEntity> type, Level world) {
 		super(type, world);
 		xpReward = 0;
 		setNoAi(false);
-
 	}
 
 	@Override
 	protected void registerGoals() {
 		super.registerGoals();
-
 		this.goalSelector.addGoal(1, new MeleeAttackGoal(this, 1.2, false) {
-
 			@Override
 			protected boolean canPerformAttack(LivingEntity entity) {
 				return this.isTimeToAttack() && this.mob.distanceToSqr(entity) < (this.mob.getBbWidth() * this.mob.getBbWidth() + entity.getBbWidth()) && this.mob.getSensing().hasLineOfSight(entity);
 			}
-
 		});
 		this.goalSelector.addGoal(2, new RandomStrollGoal(this, 1));
 		this.targetSelector.addGoal(3, new HurtByTargetGoal(this));
 		this.goalSelector.addGoal(4, new RandomLookAroundGoal(this));
 		this.goalSelector.addGoal(5, new FloatGoal(this));
-
 	}
 
 	@Override
@@ -52,7 +74,7 @@ public class TestEntityEntity extends Monster {
 	@Override
 	public SpawnGroupData finalizeSpawn(ServerLevelAccessor world, DifficultyInstance difficulty, MobSpawnType reason, @Nullable SpawnGroupData livingdata) {
 		SpawnGroupData retval = super.finalizeSpawn(world, difficulty, reason, livingdata);
-		TestEntityOnInitialEntitySpawnProcedure.execute();
+		TestEntityOnInitialEntitySpawnProcedure.execute(world, this);
 		return retval;
 	}
 
@@ -60,7 +82,6 @@ public class TestEntityEntity extends Monster {
 		event.register(AlwaysSkibidiModEntities.TEST_ENTITY.get(), SpawnPlacementTypes.ON_GROUND, Heightmap.Types.MOTION_BLOCKING_NO_LEAVES,
 				(entityType, world, reason, pos, random) -> (world.getDifficulty() != Difficulty.PEACEFUL && Monster.isDarkEnoughToSpawn(world, pos, random) && Mob.checkMobSpawnRules(entityType, world, reason, pos, random)),
 				SpawnPlacementRegisterEvent.Operation.REPLACE);
-
 	}
 
 	public static AttributeSupplier.Builder createAttributes() {
@@ -70,10 +91,7 @@ public class TestEntityEntity extends Monster {
 		builder = builder.add(Attributes.ARMOR, 0);
 		builder = builder.add(Attributes.ATTACK_DAMAGE, 3);
 		builder = builder.add(Attributes.FOLLOW_RANGE, 16);
-
 		builder = builder.add(Attributes.STEP_HEIGHT, 0.6);
-
 		return builder;
 	}
-
 }
