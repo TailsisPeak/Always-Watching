052567e95177ba0673eff531eff874da17fa99e3
KK1212KK <Conner@DESKTOP-QN5VVIO.lan>
2024-09-08 13:22:14 -0700

    random changes -cjs

---
+++ src/main/java/net/mcreator/alwaysskibidi/init/AlwaysSkibidiModModels.java
@@ -0,0 +1,20 @@
+
+/*
+ *    MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.alwaysskibidi.init;
+
+import net.neoforged.neoforge.client.event.EntityRenderersEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.api.distmarker.Dist;
+
+import net.mcreator.alwaysskibidi.client.model.ModelADVheadgear;
+
+@EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD, value = {Dist.CLIENT})
+public class AlwaysSkibidiModModels {
+	@SubscribeEvent
+	public static void registerLayerDefinitions(EntityRenderersEvent.RegisterLayerDefinitions event) {
+		event.registerLayerDefinition(ModelADVheadgear.LAYER_LOCATION, ModelADVheadgear::createBodyLayer);
+	}
+}
--- src/main/java/net/mcreator/alwaysskibidi/item/ADVgooglesItem.java
+++ src/main/java/net/mcreator/alwaysskibidi/item/ADVgooglesItem.java
@@ -3,6 +3,7 @@ package net.mcreator.alwaysskibidi.item;
 
 import net.neoforged.neoforge.registries.RegisterEvent;
 import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.bus.api.SubscribeEvent;
 
@@ -11,16 +12,25 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ArmorItem;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.Holder;
+import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.model.HumanoidModel;
+import net.minecraft.client.Minecraft;
 import net.minecraft.Util;
 
 import net.mcreator.alwaysskibidi.init.AlwaysSkibidiModItems;
+import net.mcreator.alwaysskibidi.client.model.ModelADVheadgear;
 
+import java.util.function.Consumer;
+import java.util.Map;
 import java.util.List;
 import java.util.EnumMap;
+import java.util.Collections;
 
 @EventBusSubscriber(bus = EventBusSubscriber.Bus.MOD)
 public abstract class ADVgooglesItem extends ArmorItem {
@@ -49,6 +59,24 @@ public abstract class ADVgooglesItem extends ArmorItem {
 	public static class Helmet extends ADVgooglesItem {
 		public Helmet() {
 			super(ArmorItem.Type.HELMET, new Item.Properties().durability(ArmorItem.Type.HELMET.getDurability(15)).fireResistant());
+		}
+
+		@Override
+		public void initializeClient(Consumer<IClientItemExtensions> consumer) {
+			consumer.accept(new IClientItemExtensions() {
+				@Override
+				public HumanoidModel getHumanoidArmorModel(LivingEntity living, ItemStack stack, EquipmentSlot slot, HumanoidModel defaultModel) {
+					HumanoidModel armorModel = new HumanoidModel(new ModelPart(Collections.emptyList(),
+							Map.of("head", new ModelADVheadgear(Minecraft.getInstance().getEntityModels().bakeLayer(ModelADVheadgear.LAYER_LOCATION)).Head, "hat", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "body",
+									new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_arm", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_arm",
+									new ModelPart(Collections.emptyList(), Collections.emptyMap()), "right_leg", new ModelPart(Collections.emptyList(), Collections.emptyMap()), "left_leg",
+									new ModelPart(Collections.emptyList(), Collections.emptyMap()))));
+					armorModel.crouching = living.isShiftKeyDown();
+					armorModel.riding = defaultModel.riding;
+					armorModel.young = living.isBaby();
+					return armorModel;
+				}
+			});
 		}
 	}
 }
